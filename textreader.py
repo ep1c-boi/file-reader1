4c2a0f7b11c6520b4f6cdd83951e84ed3e1f096d
epic <nilo4life@gmail.com>
2026-07-02 11:26:02 -0400

    textreader v2
    Added fallbacks for invalid file types and permissions, added more comments.

--- textreader.py
+++ textreader.py
@@ -5,17 +5,30 @@ import time
 
 #main menu
 def mainmenu():
-  print("File Reader by ep1c-boi, V1")
+  print("File Reader by ep1c-boi, v2")
   print(r"To use, insert a full file path 'example: C:\Users\user\Documents\note.txt'")
   file = input("Please enter file name. ")
   
   #innards
   if os.path.exists(file):
-      #file viewer
-      opened = open(file)
-
-      readed = opened.read()
-      print(readed)
+      #file viewer 
+      try:
+        opened = open(file)
+        readed = opened.read()
+        print(readed)
+      #error messages
+      #if its an invalid file type
+      except UnicodeDecodeError:
+        print("Entered path is not a text file.")
+        time.sleep(5)
+        os.system('cls')
+        return mainmenu()
+      #if the user doesnt have perms
+      except PermissionError:
+        print("Unauthorized to open path.")
+        time.sleep(5)
+        os.system('cls')
+        return mainmenu()
   else:
       #fail
       print("That file doesnt exist!")
@@ -26,3 +39,4 @@ def mainmenu():
 
 #extra
 mainmenu()
+
